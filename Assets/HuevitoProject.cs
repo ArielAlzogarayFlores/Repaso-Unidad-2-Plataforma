eb2f3c27c7930342c0b19c541c2e7687c2dee40e
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-05-24 09:43:33 -0300

    Initial commit

---
+++ Assets/HuevitoProject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuevitoProject : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject EggSpawnPoint;
+    int tamaño = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(prefab);
+            clon.transform.position = EggSpawnPoint.transform.position;
+            prefab.transform.localScale += new Vector3 (tamaño, tamaño, tamaño);
+        }
+    }
+}
---
+++ Assets/playerControler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour
+{
+    public float zSpeed;
+    public float RotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (transform.position.x < 4.5f)
+        {
+            //transform.position += new Vector3(0, 0, zSpeed);
+            //transform.Translate(0, 0, zSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, zSpeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -zSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -RotationSpeed, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, RotationSpeed, 0);
+        }
+        
+
+
+    }
+}
